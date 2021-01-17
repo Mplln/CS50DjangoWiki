7ddf8b64aa8909d8a40908a1f611192c000bd4be
Matheus Pellon <matheuspellon@hotmail.com>
2021-01-16 23:55:57 -0300

    implemented the search system

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'encyclopedia'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.content, name="content")
+    path("content/<str:name>", views.content, name="content"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,47 @@
+from django import forms
 from django.shortcuts import render
 from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 import markdown
 md = markdown.Markdown()
 
 
 from . import util
 
+entries = util.list_entries()
+
+class searchForm(forms.Form):
+    searchInput = forms.CharField(
+        label="Search",
+        widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'})
+        )
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries,
+        "form": searchForm()
+    })
+
+def search(request):
+    if request.method == "POST":
+        form = searchForm(request.POST)
+        results = []
+        if form.is_valid():
+            search = form.cleaned_data["searchInput"]
+            for entry in entries:
+                if entry.lower() == search.lower():
+                    return HttpResponseRedirect(reverse('encyclopedia:content', args=[entry]))
+                elif search.lower() in entry.lower():
+                    results.append(entry)
+        else:
+            return render(request, "encyclopedia/search.html", {
+                "form": form
+            })
+
+    return render(request, "encyclopedia/search.html", {
+        "form": searchForm(),
+        "results": results
     })
 
 def content(request, name):
@@ -17,7 +49,8 @@ def content(request, name):
         raise Http404("Page does not exist")
     else:
         return render(request, "encyclopedia/content.html", {
-            "entries": util.list_entries(),
+            "entries": entries,
+            "form": searchForm(),
             "name": name,
             "text": md.convert(util.get_entry(name))
         })
